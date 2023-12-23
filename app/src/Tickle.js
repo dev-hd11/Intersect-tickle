7b7ccde9c8198aaaf24400dfb2be0699bc263181
dev-hd11 <himankdeka16@gmail.com>
2023-12-23 14:16:56 +0530

    v0.4.0

--- app/src/Tickle.js
+++ app/src/Tickle.js
@@ -1,11 +1,49 @@
 import './Tickle.css';
+import { useState } from 'react';
 
-function Tickle() {
+function Heading() {
+  const [active, setActive] = useState(false);
+  const player = document.getElementById("bg-aud");
+  const [spanClass, setSpanClass] = useState("fa fa-play");
+
+  function play_bg() {
+    if (!active) {
+      setSpanClass("fa fa-pause");
+      player.play();
+      
+    } else {
+      setSpanClass("fa fa-play");
+      player.pause();
+    }
+
+    setActive(!active);
+  }
+
   return (
     <div className='head'>
+      <button id='home'>Home <span className='fa fa-home'></span></button>
       <h1>Tickle</h1>
+      <button id='audio-btn' onClick={play_bg}>Audio <span className={spanClass}></span></button>
     </div>
   );
 }
 
-export default Tickle;
+function DashBoard() {
+  return (
+    <div className='dash'>
+      <p id='player'>You</p>
+      <p id='opp'>AI</p>
+    </div>
+  );
+}
+
+function MainScreen() {
+  return(
+    <>
+      <Heading />
+      <DashBoard />
+    </>
+  );
+}
+
+export default MainScreen;
--- app/src/index.js
+++ app/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Tickle from './Tickle';
+import reportWebVitals from "./reportWebVitals"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,3 +11,4 @@ root.render(
   </React.StrictMode>
 );
 
+reportWebVitals()
